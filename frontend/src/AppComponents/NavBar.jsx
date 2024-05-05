1f999e453ce35a6fd02dd083791e5bb33a42eeff
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-05-05 21:31:16 +0530

    fix

--- frontend/src/AppComponents/NavBar.jsx
+++ frontend/src/AppComponents/NavBar.jsx
@@ -40,8 +40,8 @@ const Navbar = () => {
 
   return (
     <>
-      <div id='navbar' className='shadow-md bg-white p-2 px-3 sticky top-0 z-50'>
-        <div className='mx-auto flex max-w-[1280px] px-20 items-center justify-between'>
+      <div id='navbar' className='shadow-md bg-white p-2 px-3 sticky top-0 z-50 overflow-x-hidden'>
+        <div className='mx-auto flex max-w-[1280px] px-10 items-center justify-between'>
           <Link to={'https://www.linkedin.com/in/krishna-vamshi-kusuma-11717b213/'} target='_blank'><img src={Logo} className='h-16' alt='Logo' /></Link>
           <div className='flex gap-2'>
             <Link target='_blank' to={'https://krishnavamshi-portfolio.netlify.app/'}>
